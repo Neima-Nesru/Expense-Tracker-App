978f2f970cf9ffc1d311d4e11201039887d305be
Neima Nesru <neimanesru34@gmail.com>
2023-08-18 09:15:48 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from './component/Expenses/Expenses';
+import React from 'react'
+const App = () => {
+  const expenses = [
+    {
+      title: 'School Fee',
+      date: new Date(2023, 1, 1),
+      amount: '4893 ETB'
+    }
+    , {
+      title: 'Transport',
+      date: new Date(2023, 10, 21),
+      amount: '893 ETB'
+    }
+    , {
+      title: 'House Rent',
+      date: new Date(2022, 2, 7),
+      amount: '9893 ETB'
+    }
+  ];
 
-function App() {
+  // alternative way to jsx
+  // return React.createElement('div', {}, React.createElement('h1', {}, 'Expense Tracker'), React.createElement(Expenses, { items: expenses }))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Expense Tracker</h1>
+      <Expenses items={expenses} />
     </div>
   );
 }
