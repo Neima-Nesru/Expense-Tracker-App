b16bc310e3ceedc0dbc324899dc6157ed057c539
Neima Nesru <neimanesru34@gmail.com>
2023-08-20 07:07:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import Expenses from './component/Expenses/Expenses';
-import React from 'react'
+import React, { useState } from 'react'
+import NewExpense from './component/NewExpense/NewExpense';
+const DUMMY_EXPENSES = [
+  {
+    id: 'e01',
+    title: 'School Fee',
+    date: new Date(2023, 1, 1),
+    amount: '4893'
+  }
+  , {
+    id: 'e02',
+    title: 'Transport',
+    date: new Date(2023, 10, 21),
+    amount: '893'
+  }
+  , {
+    id: 'e03',
+    title: 'House Rent',
+    date: new Date(2022, 2, 7),
+    amount: '9893'
+  }
+];
 const App = () => {
-  const expenses = [
-    {
-      title: 'School Fee',
-      date: new Date(2023, 1, 1),
-      amount: '4893 ETB'
-    }
-    , {
-      title: 'Transport',
-      date: new Date(2023, 10, 21),
-      amount: '893 ETB'
-    }
-    , {
-      title: 'House Rent',
-      date: new Date(2022, 2, 7),
-      amount: '9893 ETB'
-    }
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
 
   // alternative way to jsx
   // return React.createElement('div', {}, React.createElement('h1', {}, 'Expense Tracker'), React.createElement(Expenses, { items: expenses }))
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+
+      return [expense, ...prevExpenses];
+    });
+
+  };
+
   return (
     <div>
       <h1>Expense Tracker</h1>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
+
     </div>
   );
 }
